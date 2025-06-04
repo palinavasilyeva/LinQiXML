dc8b039465523cf025475bdbbccd5c136d950801
palinavasilyeva <palina.vasilyeva@microsoft.wsei.edu.pl>
2025-06-04 22:32:18 +0200

    1

--- LinQiXML/Program.cs
+++ LinQiXML/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Pobierz rok ze standardowego wejścia
+        int year = int.Parse(Console.ReadLine());
+
+        // Pobierz i załaduj XML
+        string xmlInput = "";
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            xmlInput += line;
+        }
+        XDocument doc = XDocument.Parse(xmlInput);
+
+        // Zapytanie LINQ
+        var result = from customer in doc.Descendants("Customer")
+            where (from order in customer.Element("Orders").Elements("Order")
+                let orderDate = DateTime.Parse(order.Element("OrderDate").Value)
+                where orderDate.Year == year
+                select order).Any()
+            orderby customer.Element("Country").Value,
+                customer.Element("City").Value,
+                customer.Element("CompanyName").Value
+            select customer.Element("CompanyName").Value;
+
+        // Wypisz wyniki
+        if (!result.Any())
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            foreach (var companyName in result)
+            {
+                Console.WriteLine(companyName);
+            }
+        }
+    }
+}
